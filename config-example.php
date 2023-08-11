5a11efcd6eff0a217961d6b1b1b7b2a55ae8c111
dannsbass <dannsbass@gmail.com>
2023-08-12 07:17:23 +0800

    delete require Dannsheet.php

--- config-example.php
+++ config-example.php
@@ -1,7 +1,6 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/src/Dannsheet.php';
 
 Dannsheet::setCredentials(__DIR__ . '/data/credentials.json');
 Dannsheet::setSpreasheetId('1_cQzy-S0YTtQOdsqoLJL0Y96N0A3x5vwIdLr6MwbgMU');
